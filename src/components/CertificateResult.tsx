386c9a071ed7ea0c0939751cdb42aa8b877dab94
Rishi Mondal <mavrickrishi@gmail.com>
2025-03-18 21:38:39 +0530

    Update vite dependency to version 6.2.2

--- src/components/CertificateResult.tsx
+++ src/components/CertificateResult.tsx
@@ -6,43 +6,404 @@ import { cn } from '../lib/utils';
 // Mock data for demonstration - replace with actual data fetching
 const mockCertificates = [
   {
-    id: 'HH-12345',
-    candidateName: 'Ramakrushna Biswal',
-    collegeName: 'GIET University',
-    certificateType: 'Offer Letter',
-    issuedDate: 'Feb 27, 2025',
-    internshipDuration: {
-      start: 'March 1, 2025',
-      end: 'May 31, 2025',
-    },
-    photo: 'https://media.licdn.com/dms/image/v2/D5603AQEp0ho2K84olQ/profile-displayphoto-shrink_800_800/B56ZPOmwuJGsAg-/0/1734338089173?e=1746057600&v=beta&t=y9vWM5QH3ARK4O9GDXcADqkizsnGuL0-EdepQBN9ifE',
-    socials: {
-      github: 'https://github.com/ram/',
-      linkedin: 'https://www.linkedin.com/in/ram',
-      email: 'mailto:ram@gmail.com',
-    },
+      "id": "HH-E5AFCA",
+      "candidateName": "Vedant Anand",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1s3bD-U0t_k7JCHVBt2YTBumJvhrBI3K_",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://GitHub.com/vedantanand17",
+          "linkedin": "https://linkedin.com/in/vedantanand17",
+          "twitter": "https://x.com/vedantsx",
+          "email": "mailto:vedantanand.in@gmail.com"
+      }
   },
-
-  // For testing the LinkedIn profile picture fetching
   {
-    id: 'HH-98765',
-    candidateName: 'John Doe',
-    collegeName: 'University of Technology',
-    certificateType: 'Internship',
-    issuedDate: 'January 15, 2025',
-    internshipDuration: {
-      start: 'December 1, 2024',
-      end: 'January 31, 2025',
-    },
-    photo: '',
-    photoSource: 'linkedin' as 'manual' | 'linkedin',
-    socials: {
-      github: 'https://github.com/johndoe',
-      linkedin: 'https://linkedin.com/in/johndoe',
-      twitter: 'https://twitter.com/johndoe',
-      email: 'mailto:john@example.com',
-    },
+      "id": "HH-F35AF0",
+      "candidateName": "Ghanshyam Singh",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1QiEQMRCdIFM_MZ8dvGSP_bhQOrldxhQa",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/ghanshyam2005singh",
+          "linkedin": "https://www.linkedin.com/in/ghanshyam-singh-b014232b2/",
+          "twitter": "https://x.com/https_ghanshyam",
+          "email": "mailto:ghanshyam2005singh@gmail.com"
+      }
   },
+  {
+      "id": "HH-B25DC8",
+      "candidateName": "Anushka Tiwari",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1o9zeIBAmPYVT2-2NFAgEpHpf0WIqBjbz",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/anushka226",
+          "linkedin": "https://www.linkedin.com/in/anushka-tiwari-a2693526b/",
+          "twitter": "https://x.com/anushkat711",
+          "email": "mailto:anushkat711@gmail.com"
+      }
+  },
+  {
+      "id": "HH-8F670E",
+      "candidateName": "Ayushmaan Singh yadav ",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1fc2_7Bd3EDB703t5HGx9o54fW6_nc1o_",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/asteriskayush007",
+          "linkedin": "https://www.linkedin.com/in/ayushmaansinghyadav/",
+          "twitter": "https://x.com/SinghYadav58685",
+          "email": "mailto:ayushmaansinghyadav18@gmail.com"
+      }
+  },
+  {
+      "id": "HH-DA503C",
+      "candidateName": "Chiranth Raju C ",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1xOCO1L3kXg9T6ewM9h4Lh5xOLbAVEBgj",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/CHIRANTH-24/",
+          "linkedin": "https://www.linkedin.com/in/chiranthrajuc/",
+          "twitter": "https://twitter.com/ChiranthRaju",
+          "email": "mailto:chiranthrajuc@gmail.com"
+      }
+  },
+  {
+      "id": "HH-5B3B2C",
+      "candidateName": "Karishma Seth",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1LBG8f18k8rkk3DsSoNwIcITCRkBufqum",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/karishmaaa101",
+          "linkedin": "https://www.linkedin.com/in/karishmaaa101/",
+          "twitter": "https://x.com/karishmaseth6",
+          "email": "mailto:motivatedme999@gmail.com"
+      }
+  },
+  {
+      "id": "HH-1CA687",
+      "candidateName": "Piyush Prasad",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1aYkrJKPUorD7-57JjsRXTT2UeTXcZUh8",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/piyzard",
+          "linkedin": "https://www.linkedin.com/in/piyzard/",
+          "twitter": "https://x.com/notpiyzard",
+          "email": "mailto:piyushprasad121@gmail.com"
+      }
+  },
+  {
+      "id": "HH-77A864",
+      "candidateName": "Yash Kumar Saini",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1cvijjT7GlWLnJyGICym3NGzqf6mnjBBH",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/yashksaini-coder",
+          "linkedin": "https://www.linkedin.com/in/yashksaini/",
+          "twitter": "https://x.com/EasycodesDev",
+          "email": "mailto:ys3853428@gmail.com"
+      }
+  },
+  {
+      "id": "HH-CEB7D7",
+      "candidateName": "Harshit Verma ",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1_fMo9Om0aOusqYG_slMWUwnPMnB2CJvE",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/therealharshit",
+          "linkedin": "https://www.linkedin.com/in/therealharshit",
+          "twitter": "https://x.com/therealharshiit",
+          "email": "mailto:Therealharshit014@gmail.com"
+      }
+  },
+  {
+      "id": "HH-A53EAA",
+      "candidateName": "Sayan Rakshit",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1e8puQrDC_DV-vl-N4w7kN1b9dzVa7BCR",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/devsayanR",
+          "linkedin": "https://www.linkedin.com/in/sayandotcom",
+          "twitter": "https://x.com/sayandotdev",
+          "email": "mailto:sayanrakshit.hpl@gmail.com"
+      }
+  },
+  {
+      "id": "HH-67E110",
+      "candidateName": "sneha kumari",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1L3-SW262tXGOue_OEdaJ8foMA6vvntu3",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/Snehsk",
+          "linkedin": "https://www.linkedin.com/in/sneha-kumari1/",
+          "twitter": "https://x.com/SnehaKumariSk1",
+          "email": "mailto:snehakumari0456@gmail.com"
+      }
+  },
+  {
+      "id": "HH-58AA0B",
+      "candidateName": "Hetvi Popat",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1xjrFDU_3S_k5zIGUYyjWc4xrWK2K0B2V",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "",
+          "linkedin": "https://www.linkedin.com/in/hetvi-popat-283b49272",
+          "twitter": "https://x.com/HetviP2605?t=ytiunxBerGKZKQ54IQtrVg&s=09",
+          "email": "mailto:phetvi.2605@gmail.com"
+      }
+  },
+  {
+      "id": "HH-8BD123",
+      "candidateName": "Manishka Asrani",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1NkfMdyJtECrq2hgA5ZG2R3ev3KFn6WHl",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/manishka119",
+          "linkedin": "https://www.linkedin.com/in/manishka-asrani/",
+          "twitter": "https://x.com/manishka119",
+          "email": "mailto:manishkaasrani@gmail.com"
+      }
+  },
+  {
+      "id": "HH-D95E84",
+      "candidateName": "Devaki Joshi",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1HW0Tuo9iyCicYH294JzMUfndPkzTMN4P",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/Devaki01",
+          "linkedin": "www.linkedin.com/in/devaki-joshi-033b6a307",
+          "twitter": "-",
+          "email": "mailto:devakijoshi.2005@gmail.com"
+      }
+  },
+  {
+      "id": "HH-73B949",
+      "candidateName": "Ramakrushna Biswal",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1bK47IC7lqZ71vfnX1fyEs9RiSyYuEH1g",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/RamakrushnaBiswal",
+          "linkedin": "https://www.linkedin.com/in/ramakrushna-biswal",
+          "twitter": "https://x.com/Ramakrushna23",
+          "email": "mailto:ramakrushnabunty@gmail.com"
+      }
+  },
+  {
+      "id": "HH-CADB54",
+      "candidateName": "Sanchit Sehgal ",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1JeBuVWSOZJbN5JFCJ2dC5Q-vx-tiOPE3",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/Sanchit-aiml",
+          "linkedin": "https://www.linkedin.com/in/sanchitsehgal/",
+          "twitter": "https://x.com/sanchitseh38640?s=11",
+          "email": "mailto:sanchitsehgal16@gmail.com"
+      }
+  },
+  {
+      "id": "HH-F42F64",
+      "candidateName": "Divyam Sharma",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1dHhTxHEGXgS1nOaVsimFqdEqFsbqOpsb",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/Divyamsharma-18",
+          "linkedin": "https://www.linkedin.com/in/divyam-sharma-6ba700249/",
+          "twitter": "https://x.com/Heydivyamsharma",
+          "email": "mailto:divyamsharmaworks@gmail.com"
+      }
+  },
+  {
+      "id": "HH-B5261E",
+      "candidateName": "Muskan kashyap ",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1JNAr5ZZnKsZl89vRIx3h5mN95FyZgb-U",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/Muskankashyap12",
+          "linkedin": "https://www.linkedin.com/in/muskan-kashyap-b367b5290?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+          "twitter": "NA",
+          "email": "mailto:muskankashyap1402@gmail.com"
+      }
+  },
+  {
+      "id": "HH-99C654",
+      "candidateName": "Saumili Dutta",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1VMzhJr5kSOpzIJ01odANCtx_MyhblONO",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/aumiidutta",
+          "linkedin": "https://www.linkedin.com/in/saumilidutta/",
+          "twitter": "https://x.com/aumiidutta",
+          "email": "mailto:saumili.work@gmail.com"
+      }
+  },
+  {
+      "id": "HH-FF837E",
+      "candidateName": "Aditya D",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1g4xsDPy_7HPN9_8MNK_uKyvvywXgoC5H",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/adi271001",
+          "linkedin": "https://www.linkedin.com/in/aditya-d-23453a179/",
+          "twitter": "https://x.com/ADITYAD29257528",
+          "email": "mailto:ibetanibex@gmail.com"
+      }
+  },
+  {
+      "id": "HH-12D9B0",
+      "candidateName": "Sanskriti",
+      "collegeName": "",
+      "certificateType": "Internship",
+      "issuedDate": "January 15, 2025",
+      "internshipDuration": {
+          "start": "December 1, 2024",
+          "end": "January 31, 2025"
+      },
+      "photo": "https://drive.google.com/open?id=1u_ORvxPHaHojAprYyZ1UJ1kN_W1SLxP0",
+      "photoSource": "linkedin",
+      "socials": {
+          "github": "https://github.com/sanskriti2005",
+          "linkedin": "https://www.linkedin.com/in/sanskriti-s/",
+          "twitter": "N/a",
+          "email": "mailto:sanskritisri27@gmail.com"
+      }
+  }
 ];
 
 export function CertificateResult() {
